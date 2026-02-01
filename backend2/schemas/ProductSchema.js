9f78fc0474c01a53d7a4fb8526ce8557fdf6c2e1
Lavanya2804-dev <lavanyasaniboeena@gmail.com>
2026-02-01 19:20:38 +0530

    added backend2

---
+++ backend2/schemas/ProductSchema.js
@@ -0,0 +1,30 @@
+import {Schema,model} from 'mongoose'
+
+//create Product schema (pid,productname,price)
+const ProductSchema=new Schema({
+    pid:{
+        type:String,
+        required:[true,"pid is required"]
+    },
+    productname:{
+        type:String,
+        required:[true,"ProductName is required"],
+        minLength:[4,"min length should be 4"],
+        maxLength:[6,"Max length exceeded"]
+
+    },
+    price:{
+        type:Number,
+        required:[true,"age is required"],
+        min:[10000,"price should be above 10000"],
+        max:[100000,"price should be less than 100000"]
+    }
+//},{
+    //strict:"throw",
+   // timestamps:true
+
+//
+});
+
+
+export const ProductModel = model("Product", ProductSchema)
