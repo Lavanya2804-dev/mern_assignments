037a04d211e6082f272f849fa027a4bf83840cd5
Lavanya2804-dev <lavanyasaniboeena@gmail.com>
2026-05-14 11:02:04 +0530

    Remove blog-app from repository

--- blog-app/Frontend/src/components/AdminDashboard.jsx
+++
@@ -1,7 +0,0 @@
-export default function AdminDashboard(){
-    return(
-        <div className="text-center mt-10">
-            <h2 className="text-2xl">Admin DashBoard</h2>
-        </div>
-    )
-}
